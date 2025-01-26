65213bca5c1fa78638ecd3a36926b39565abacaa
hsjoihs / sozysozbot <sozysozbot@gmail.com>
2025-01-26 19:21:53 +0900

    Migrate to TypeScript

--- ts-src/audio.ts
+++ ts-src/audio.ts
@@ -1,4 +1,3 @@
-"use strict";
 function beepSound() {
     const audioContext = new window.AudioContext();
     const oscillator = audioContext.createOscillator();
@@ -11,28 +10,36 @@ function beepSound() {
     oscillator.start();
     oscillator.stop(audioContext.currentTime + 0.2);
 }
+
 function clickSound() {
     const audioContext = new window.AudioContext();
+
     const bufferSize = audioContext.sampleRate * 0.05 /* seconds */;
     const buffer = audioContext.createBuffer(1, bufferSize, audioContext.sampleRate);
     const data = buffer.getChannelData(0);
     for (let i = 0; i < bufferSize; i++) {
         data[i] = Math.random() * 2 - 1;
     }
+
     const noise = audioContext.createBufferSource();
     noise.buffer = buffer;
+
     const highPass = audioContext.createBiquadFilter();
     highPass.type = "highpass";
     highPass.frequency.setValueAtTime(500 /*Hz*/, audioContext.currentTime);
+
     const lowPass = audioContext.createBiquadFilter();
     lowPass.type = "lowpass";
     lowPass.frequency.setValueAtTime(1500 /*Hz*/, audioContext.currentTime);
+
     const gainNode = audioContext.createGain();
     gainNode.gain.setValueAtTime(0.9, audioContext.currentTime);
-    gainNode.gain.exponentialRampToValueAtTime(0.01, audioContext.currentTime + 0.05 /* seconds; decay*/);
+    gainNode.gain.exponentialRampToValueAtTime(0.01, audioContext.currentTime + 0.05/* seconds; decay*/);
+
     noise.connect(highPass);
     highPass.connect(lowPass);
     lowPass.connect(gainNode);
     gainNode.connect(audioContext.destination);
+
     noise.start();
 }
--- ts-src/main.ts
+++ ts-src/main.ts
@@ -1,96 +1,104 @@
-"use strict";
 let cursoredBox = 0;
+
 // get the currently highlighted input element
-function getHighlightedInput() {
-    const v = document.querySelector("input:focus");
+function getHighlightedInput(): HTMLInputElement {
+    const v = document.querySelector("input:focus") as HTMLInputElement;
     if (!v) {
-        return document.getElementById("codepoint" + cursoredBox);
+        return document.getElementById("codepoint" + cursoredBox)! as HTMLInputElement;
     }
     return v;
 }
-function insertAtCursor(textToInsert) {
+
+function insertAtCursor(textToInsert: string) {
     const inputElement = getHighlightedInput();
     const currentValue = inputElement.value;
     const start = inputElement.selectionStart ?? 0;
     const end = inputElement.selectionEnd ?? 0;
     inputElement.value = currentValue.slice(0, start) + textToInsert + currentValue.slice(end);
     inputElement.setSelectionRange(start + textToInsert.length, start + textToInsert.length);
+
     // If this completes the codepoint, move to the next input
     if (inputElement.value.length === 1 /*TODO*/) {
         cursoredBox = (cursoredBox + 1) % 2 /* TODO */;
-        const nextInput = document.getElementById("codepoint" + cursoredBox);
+        const nextInput = document.getElementById("codepoint" + cursoredBox)! as HTMLInputElement;
         nextInput.focus();
         nextInput.setSelectionRange(0, 0);
     }
+
     const previousValue = currentValue;
     syncAll_triggered_from_custom_keyboard(previousValue);
 }
-function sync_triggered_from_textinput(i) {
+
+function sync_triggered_from_textinput(i: number) {
     let success = true;
-    const codepointInput = document.getElementById("codepoint" + i);
+    const codepointInput = document.getElementById("codepoint" + i)! as HTMLInputElement;
+
     const codepoint = codepointInput.value;
-    const char_div = document.getElementById("char" + i);
+    const char_div = document.getElementById("char" + i)!;
+
     // Delete offending characters
     if (!codepointInput.checkValidity()) {
         if (success) {
             beepSound();
             success = false;
         }
+
         codepointInput.value = "";
         char_div.textContent = "";
         char_div.classList.add("unfilled-char-cell");
-    }
-    else if (codepoint === "") {
+    } else if (codepoint === "") {
         char_div.textContent = "";
         char_div.classList.add("unfilled-char-cell");
-    }
-    else {
+    } else {
         char_div.textContent = String.fromCodePoint(parseInt("306" + codepoint, 16));
         char_div.classList.remove("unfilled-char-cell");
+
         // If this completes the codepoint, move to the next input
         if (codepoint.length === 1 /*TODO*/) {
             cursoredBox = (i + 1) % 2 /* TODO */;
-            const nextInput = document.getElementById("codepoint" + cursoredBox);
+            const nextInput = document.getElementById("codepoint" + cursoredBox)! as HTMLInputElement;
             nextInput.focus();
             nextInput.setSelectionRange(0, 0);
         }
     }
+
     if (success) {
         clickSound();
     }
 }
-function syncAll_triggered_from_custom_keyboard(previousValue) {
+
+function syncAll_triggered_from_custom_keyboard(previousValue: string) {
     let success = true;
     for (let i = 0; i < 2; i++) {
-        const codepointInput = document.getElementById("codepoint" + i);
+        const codepointInput = document.getElementById("codepoint" + i)! as HTMLInputElement;
         const codepoint = codepointInput.value;
-        const char_div = document.getElementById("char" + i);
+        const char_div = document.getElementById("char" + i)!;
+
         // Delete offending characters
         if (!codepointInput.checkValidity()) {
             if (success) {
                 beepSound();
                 success = false;
             }
+
             if (previousValue === undefined) {
                 codepointInput.value = "";
                 char_div.textContent = "";
                 char_div.classList.add("unfilled-char-cell");
-            }
-            else {
+            } else {
                 codepointInput.value = previousValue;
                 char_div.textContent = String.fromCodePoint(parseInt("306" + previousValue, 16));
                 char_div.classList.remove("unfilled-char-cell");
             }
-        }
-        else if (codepoint === "") {
+        } else if (codepoint === "") {
             char_div.textContent = "";
             char_div.classList.add("unfilled-char-cell");
-        }
-        else {
+        } else {
             char_div.textContent = String.fromCodePoint(parseInt("306" + codepoint, 16));
             char_div.classList.remove("unfilled-char-cell");
         }
     }
+
     if (success) {
         clickSound();
     }
